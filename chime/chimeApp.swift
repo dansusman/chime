f765839aa92dd781ade6c32f309af1d10b097218
Dan Susman <frenchminister@gmail.com>
2024-07-04 18:30:01 -0400

    Initial Commit

---
+++ chime/chimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  chimeApp.swift
+//  chime
+//
+//  Created by Dan Susman on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct chimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
