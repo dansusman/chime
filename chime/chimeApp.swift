00973b3afea8229c632bf519fd23d48ad4b9ecce
Dan Susman <frenchminister@gmail.com>
2024-07-04 20:07:46 -0400

    Initial commit

--- chime/chimeApp.swift
+++ chime/chimeApp.swift
@@ -5,13 +5,29 @@
 //  Created by Dan Susman on 7/4/24.
 //
 
+import AVFAudio
+import Combine
 import SwiftUI
 
 @main
 struct chimeApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra("Chime", systemImage: "bell.circle") {
+            EmptyView().onReceive(ChimeUtil.timer) { _ in ChimeUtil.playSound() }
         }
+        .menuBarExtraStyle(.window)
+    }
+}
+
+struct ChimeUtil {
+    static var timer: Publishers.Autoconnect<Timer.TimerPublisher> {
+        let interval = 60
+        return Timer.publish(every: TimeInterval(interval),
+                             on: .main,
+                             in: .common).autoconnect()
+    }
+
+    static func playSound() {
+        NSSound(named: "chime1")?.play()
     }
 }
